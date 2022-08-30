28d17e0eb194be0e6fd610d9374b372d4189554b
Lyze <11274700+lyze237@users.noreply.github.com>
2022-08-30 09:31:18 +0200

    Replaced more high level java code

--- src/main/java/io/github/orioncraftmc/meditate/internal/YGLayout.java
+++ src/main/java/io/github/orioncraftmc/meditate/internal/YGLayout.java
@@ -39,7 +39,12 @@ public class YGLayout {
 
     @NotNull
     private ArrayList<Float> createEmptyFloatArray() {
-        return new ArrayList<>(List.of(0f, 0f, 0f, 0f));
+        List<Float> floats = new ArrayList<>();
+        floats.add(0f);
+        floats.add(0f);
+        floats.add(0f);
+        floats.add(0f);
+        return new ArrayList<>(floats);
     }
 
     public final YGDirection direction() {
