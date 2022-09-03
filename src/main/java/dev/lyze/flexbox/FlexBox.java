166da8ef094e5ab2707775c17aa35d0b3314c793
raeleus <raymond.ray3k@gmail.com>
2022-09-03 16:56:45 -0700

    Formatting and added JavaDocs.

--- src/main/java/dev/lyze/flexbox/FlexBox.java
+++ src/main/java/dev/lyze/flexbox/FlexBox.java
@@ -9,6 +9,18 @@ import io.github.orioncraftmc.meditate.YogaConfigFactory;
 import io.github.orioncraftmc.meditate.YogaNode;
 import io.github.orioncraftmc.meditate.YogaNodeFactory;
 
+/**
+ * A Scene2D widget that implements Yoga Layout by Facebook. FlexBox is a clean and powerful alternative to group
+ * widgets like {@link com.badlogic.gdx.scenes.scene2d.ui.Table} and
+ * {@link com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup}. {@link #add(Actor) Add} an {@link Actor} to the widget
+ * and use the returned {@link YogaNode} to modify its FlexBox properties. {@link #getRoot() Get the root node} to
+ * modify the properties of the top level element.
+ *
+ * @author Lyze
+ * @see <a href="https://yogalayout.com/docs/">Yoga documentation</a>
+ * @see <a href="https://yogalayout.com/playground/">Yoga playground</a>
+ * @see <a href="https://github.com/raeleus/meditate-layout">Meditate Layout brought to libGDX by LyzeDev</a>
+ */
 public class FlexBox extends WidgetGroup {
     private final YogaConfig config;
     private final YogaNode root;
@@ -19,11 +31,18 @@ public class FlexBox extends WidgetGroup {
     private float prefWidth, prefHeight;
     private float lastPrefWidth, lastPrefHeight;
     
+    /**
+     * Creates a FlexBox with the default configuration.
+     */
     public FlexBox() {
         this.config = YogaConfigFactory.create();
         this.root = YogaNodeFactory.create();
     }
     
+    /**
+     * Creates a FlexBox with the provided {@link YogaConfig}.
+     * @param config
+     */
     public FlexBox(YogaConfig config) {
         this.config = config;
         this.root = YogaNodeFactory.create();
@@ -50,19 +69,43 @@ public class FlexBox extends WidgetGroup {
             actor.setBounds(node.getLayoutX(), getHeight() - node.getLayoutY() - node.getLayoutHeight(), node.getLayoutWidth(), node.getLayoutHeight());
         }
     }
-
+    
+    /**
+     * Adds an actor to the end of the root elements list.
+     * @param actor The {@link Actor Scene2D Actor} to be added to the list.
+     * @return The node associated with the actor. Change the properties of the node to modify the FlexBox layout.
+     */
     public YogaNode add(Actor actor) {
         return addAt(actor, root.getChildCount());
     }
-
+    
+    /**
+     * Adds an actor to the specified position in the root elements list.
+     * @param actor The {@link Actor Scene2D Actor} to be added to the list.
+     * @param i The position to place the actor.
+     * @return The node associated with the actor. Change the properties of the node to modify the FlexBox layout.
+     */
     public YogaNode addAt(Actor actor, int i) {
         return addAsChild(root, actor, i);
     }
-
+    
+    /**
+     * Adds an actor as a child to the specified node at the end of the elements list.
+     * @param parent The parent node that the actor will be added as a child to.
+     * @param actor The {@link Actor Scene2D Actor} to be added.
+     * @return The node associated with the actor. Change the properties of the node to modify the FlexBox layout.
+     */
     public YogaNode addAsChild(YogaNode parent, Actor actor) {
         return addAsChild(parent, actor, parent.getChildCount());
     }
-
+    
+    /**
+     * Adds an actor as a child to the specified node to the specified position of the elements list.
+     * @param parent The parent node that the actor will be added as a child to.
+     * @param actor The {@link Actor Scene2D Actor} to be added.
+     * @param i The position to place the actor.
+     * @return The node associated with the actor. Change the properties of the node to modify the FlexBox layout.
+     */
     public YogaNode addAsChild(YogaNode parent, Actor actor, int i) {
         YogaNode node = YogaNodeFactory.create(config);
 
@@ -82,7 +125,11 @@ public class FlexBox extends WidgetGroup {
 
         return node;
     }
-
+    
+    /**
+     * Removes the specified node and the {@link Actor} associated with it.
+     * @param node
+     */
     public void remove(YogaNode node) {
         YogaNode parent = node.getOwner();
 
@@ -101,7 +148,11 @@ public class FlexBox extends WidgetGroup {
             }
         }
     }
-
+    
+    /**
+     * Removes all child actors and nodes.
+     */
+    @Override
     public void clear() {
         for (int i = root.getChildCount() - 1; i >= 0; i--)
             root.removeChildAt(i);
@@ -129,9 +180,19 @@ public class FlexBox extends WidgetGroup {
             return actor;
         }
     }
-
+    
+    /**
+     * Returns the root element of the FlexBox.
+     * @return The root element.
+     */
     public YogaNode getRoot() {
         return root;
+    }
+    
+    @Override
+    public void invalidate() {
+        super.invalidate();
+        prefSizeInvalid = true;
     }
     
     @Override
@@ -146,15 +207,10 @@ public class FlexBox extends WidgetGroup {
         return prefHeight;
     }
     
-    @Override
-    public void invalidate() {
-        super.invalidate();
-        prefSizeInvalid = true;
-    }
-    
     private void calcPrefSize() {
         prefSizeInvalid = false;
         
+        //calculate the smallest resulting width at the current height
         root.calculateLayout(0, getHeight());
         float maxWidth = 0;
         for (YogaActor yogaActor : nodes) {
@@ -163,6 +219,7 @@ public class FlexBox extends WidgetGroup {
         }
         prefWidth = maxWidth;
         
+        //calculate the smallest resulting height at the current width;
         root.calculateLayout(getWidth(), 0);
         float maxHeight = 0;
         for (YogaActor yogaActor : nodes) {
